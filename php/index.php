cfc6f86d5f31c7eba0de3eb860ecd9b6bbad5432
Yordan Iliev <jordan.iliev501@gmail.com>
2025-01-24 18:12:32 +0200

    Make compatible with xampp

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION["user_faculty_number"])) {
     // fetch the user's forms
     require 'utils/db_connection.php';
-    $stmt = $conn->prepare("SELECT id, form_definition->>'$.title' AS 'title', (SELECT count(*) from responses r where r.form_id = f.id) AS 'response_count' FROM forms f WHERE author_fn = ?");
+    $stmt = $conn->prepare("SELECT id, JSON_UNQUOTE(JSON_EXTRACT(form_definition, '$.title')) AS title, (SELECT count(*) from responses r where r.form_id = f.id) AS 'response_count' FROM forms f WHERE author_fn = ?");
     $stmt->bind_param("s", $_SESSION["user_faculty_number"]);
     $stmt->execute();
     $user_forms = $stmt->get_result();

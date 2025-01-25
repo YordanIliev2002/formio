d1418d5505684e87b1d28b544626ca3b6e6ec5ad
Yordan Iliev <jordan.iliev501@gmail.com>
2025-01-25 16:44:23 +0200

    Prevent duplicate submissions

--- php/form.php
+++ php/form.php
@@ -2,10 +2,20 @@
 require 'utils/assert_user_is_logged_in.php';
 require 'utils/form_field_visualisations.php';
 
+function hasUserFilledForm($formId) {
+    require 'utils/db_connection.php';
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM responses WHERE form_id = ? AND author_fn = ?");
+    $stmt->bind_param("is", $formId, $_SESSION["user_faculty_number"]);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+    return $count > 0;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // TODO - validate the form
 
-    require 'utils/db_connection.php';
     require 'utils/queries.php';
     $formDefinition = getFormDefinition($_POST["form_id"]);
 
@@ -25,13 +35,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
     $response = json_encode($fieldsAndValues, JSON_UNESCAPED_UNICODE);
-    echo $response;
 
+    require 'utils/db_connection.php';
     $stmt = $conn->prepare("INSERT INTO responses (form_id, author_fn, response) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $_POST["form_id"], $_SESSION["user_faculty_number"], $response);
     $stmt->execute();
     $stmt->close();
-    header("Location: index.php");
+    header("Location: form.php?id=" . $_POST["form_id"] . "&success=1");
     exit;
 } else if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!isset($_GET["id"])) {
@@ -58,7 +68,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <p>
         <?= htmlspecialchars($formDefinition["description"]) ?>
     </p>
-    <?php if (!isset($_GET["access_code"]) && isset($formDefinition["accessCode"])): ?>
+    <?php if (hasUserFilledForm($_GET["id"])): ?>
+        <?php if (isset($_GET["success"])): ?>
+            <p>Thank you for filling out this form!</p>
+        <?php else: ?>
+            <p>You have already filled this form.</p>
+        <?php endif; ?>
+        <a href="index.php">Return to Home</a>
+    <?php elseif (!isset($_GET["access_code"]) && isset($formDefinition["accessCode"])): ?>
         <?php if (isset($_GET["access_code_error"])): ?>
             <p style="color: red;">Access code is incorrect. Please try again.</p>
         <?php endif; ?>

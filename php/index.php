554c212c427bdeca1d458a00c162bb19f6ca3150
Yordan Iliev <jordan.iliev501@gmail.com>
2025-01-27 22:05:47 +0200

    Show access code in home page

--- php/index.php
+++ php/index.php
@@ -3,7 +3,13 @@ session_start();
 if (isset($_SESSION["user_faculty_number"])) {
     // fetch the user's forms
     require 'utils/db_connection.php';
-    $stmt = $conn->prepare("SELECT id, JSON_UNQUOTE(JSON_EXTRACT(form_definition, '$.title')) AS title, (SELECT count(*) from responses r where r.form_id = f.id) AS 'response_count' FROM forms f WHERE author_fn = ?");
+    $stmt = $conn->prepare(
+        "SELECT
+        id,
+        JSON_UNQUOTE(JSON_EXTRACT(form_definition, '$.title')) AS title,
+        JSON_UNQUOTE(JSON_EXTRACT(form_definition, '$.accessCode')) AS access_code,
+        (SELECT count(*) from responses r where r.form_id = f.id) AS response_count
+        FROM forms f WHERE author_fn = ?");
     $stmt->bind_param("s", $_SESSION["user_faculty_number"]);
     $stmt->execute();
     $user_forms = $stmt->get_result();
@@ -35,6 +41,7 @@ if (isset($_SESSION["user_faculty_number"])) {
                     <th>Form name</th>
                     <th>Open</th>
                     <th>Copy URL</th>
+                    <th>Access code</th>
                     <th>Submissions</th>
                     <th>Invite users</th>
                     <th>Statistics</th>
@@ -49,7 +56,14 @@ if (isset($_SESSION["user_faculty_number"])) {
                     <tr>
                     <td><?= htmlspecialchars($row["title"]) ?></td>
                     <td><button onclick="location.href='/form.php?id=<?= $row["id"] ?>'" class="primary-button">Open</button></td>
-                    <td><button id="copy-button-<?= $row["id"]?>"onclick="onCopyButtonClick('<?= $row["id"]?>', '<?= $form_url ?>')" class="primary-button">Copy URL</button></td>
+                    <td><button id="copy-button-<?= $row["id"]?>" onclick="onCopyButtonClick('<?= $row["id"]?>', '<?= $form_url ?>')" class="primary-button">Copy URL</button></td>
+                    <td>
+                        <?php if ($row["access_code"]): ?>
+                            <p accesscode="<?= $row["access_code"] ?>">Hover to reveal</p>
+                        <?php else: ?>
+                            <p>-</p>
+                        <?php endif; ?>
+                    </td>
                     <td><p id="filled"><?= $row["response_count"] ?></p></td>
                     <td><a href="invite_users.php?form_id=<?= $row["id"] ?>" class="primary-button">Invite users</a></td>
                     <td><a href="statistics.php?form_id=<?= $row["id"] ?>" class="primary-button">Statistics</a></td>
@@ -83,6 +97,20 @@ if (isset($_SESSION["user_faculty_number"])) {
                 button.innerHTML = 'Copy URL';
             }, 1000);
         }
+
+        (() => {
+            const accessCodes = document.querySelectorAll('[accesscode]');
+            accessCodes.forEach((accessCode) => {
+                const code = accessCode.getAttribute('accesscode');
+                accessCode.textContent = "Hover to reveal";
+                accessCode.addEventListener('mouseover', () => {
+                    accessCode.textContent = code;
+                });
+                accessCode.addEventListener('mouseout', () => {
+                    accessCode.textContent = "Hover to reveal";
+                });
+            });
+        })();
     </script>
 </body>
 
